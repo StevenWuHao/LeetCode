95c6159357161417e539e90b4fae366f318c7ba2
golang <342556278@qq.com>
2019-12-10 17:38:51 +0800

    编写一个函数来查找字符串数组中的最长公共前缀

--- zhangzhihua/longestCommonPrefix.go
+++ zhangzhihua/longestCommonPrefix.go
@@ -24,11 +24,38 @@ import "fmt"
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 func main() {
-	strs := []string{"flower", "flow", "flight"}
+
+	//strs := []string{"flower", "flow", "flight", "fly"}
+	//strs := []string{"flower", "flower", "flower", "flewer"}
+	strs := []string{"aa", "a"}
 	fmt.Println(longestCommonPrefix(strs))
 }
 
 func longestCommonPrefix(strs []string) string {
 
-	return " "
+	strsLen := len(strs)
+
+	//如果传入空数组，直接返回空字符串
+	if strsLen == 0 {
+		return ""
+	}
+
+	//如果传入的数据长度=1，则直接返回这个值的第一个值
+	if strsLen == 1 {
+		return strs[0]
+	}
+	//把数组的第一位作为模版
+	first := strs[0]
+
+	for f := 0; f < len(first); f++ {
+		//从数组第二位开始
+		for i := 1; i < strsLen; i++ {
+			//如果和后面一位不同，说明直接可以返回了返回长度就是模版的F字符串位置
+			if f == len(strs[i]) || first[f] != strs[i][f] {
+				return first[0:f]
+			}
+		}
+	}
+
+	return first
 }
