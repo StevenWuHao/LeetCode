360a1dbbab8c69f68911759f6abb4ad96a66da00
golang <342556278@qq.com>
2019-12-25 11:29:39 +0800

    执行用时 : 0 ms , 在所有 golang 提交中击败了 100.00% 的用户 内存消耗 : 2.1 MB , 在所有 golang 提交中击败了 100.00% 的用户

--- zhangzhihua/toGoatLatin.go
+++ zhangzhihua/toGoatLatin.go
@@ -40,6 +40,17 @@ import (
 func main() {
 
 	s := "I speak Goat Latin"
+
+	//执行用时 :
+	//	0 ms
+	//	, 在所有 golang 提交中击败了
+	//	100.00%
+	//		的用户
+	//内存消耗 :
+	//	2.1 MB
+	//	, 在所有 golang 提交中击败了
+	//	100.00%
+	//		的用户
 	fmt.Println(toGoatLatin(s))
 }
 
