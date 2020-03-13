36f74abce8f3d0d61fbaa7512350de98a13d0575
golang <342556278@qq.com>
2020-03-13 15:13:05 +0800

    无变化

--- zhangzhihua/tree/levelOrder_test.go
+++ zhangzhihua/tree/levelOrder_test.go
@@ -12,5 +12,5 @@ func TestLevelOrder(t *testing.T) {
 		Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15, Left: nil, Right: nil}, Right: &TreeNode{Val: 7, Left: nil, Right: nil}}}
 
 	//fmt.Println(tn)
-	fmt.Println(levelOrder(tn))
+	fmt.Println(levelOrderBottom(tn))
 }
